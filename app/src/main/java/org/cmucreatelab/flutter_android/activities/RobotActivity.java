1023095d31f7910bb4bebb0861f8e8453025d9b1
Steve Fulton <sjf1008@sru.edu>
2016-10-17 11:46:06 -0400

    On click listeners for the robot page

--- app/src/main/java/org/cmucreatelab/flutter_android/activities/RobotActivity.java
+++ app/src/main/java/org/cmucreatelab/flutter_android/activities/RobotActivity.java
@@ -3,13 +3,16 @@ package org.cmucreatelab.flutter_android.activities;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
 import org.cmucreatelab.flutter_android.R;
 import org.cmucreatelab.flutter_android.activities.abstract_activities.BaseNavigationActivity;
 import org.cmucreatelab.flutter_android.helpers.GlobalHandler;
+import org.cmucreatelab.flutter_android.helpers.static_classes.Constants;
 import org.cmucreatelab.flutter_android.ui.dialogs.NoFlutterConnectedDialog;
 
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class RobotActivity extends BaseNavigationActivity {
 
@@ -33,4 +36,47 @@ public class RobotActivity extends BaseNavigationActivity {
             noFlutterConnectedDialog.show(getSupportFragmentManager(), "tag");
         }
     }
+
+
+    @OnClick(R.id.image_servo_1)
+    public void onClickServo1() {
+        Log.d(Constants.LOG_TAG, "onClickServo1");
+    }
+
+
+    @OnClick(R.id.image_servo_2)
+    public void onClickServo2() {
+        Log.d(Constants.LOG_TAG, "onClickServo2");
+    }
+
+
+    @OnClick(R.id.image_servo_3)
+    public void onClickServo3() {
+        Log.d(Constants.LOG_TAG, "onClickServo3");
+    }
+
+
+    @OnClick(R.id.image_led_1)
+    public void onClickLed1() {
+        Log.d(Constants.LOG_TAG, "onClickLed1");
+    }
+
+
+    @OnClick(R.id.image_led_2)
+    public void onClickLed2() {
+        Log.d(Constants.LOG_TAG, "onClickLed2");
+    }
+
+
+    @OnClick(R.id.image_led_3)
+    public void onClickLed3() {
+        Log.d(Constants.LOG_TAG, "onClickLed3");
+    }
+
+
+    @OnClick(R.id.image_speaker)
+    public void onClickSpeaker() {
+        Log.d(Constants.LOG_TAG, "onClickSpeaker");
+    }
+
 }
