ceb8c006853c7ffb817a52e1f786f3040dcfe46b
mtasota <miketasota@gmail.com>
2016-12-01 15:03:14 -0500

    Updated NamingHandler with new naming algorithm (and some minor structure changes).

--- app/src/main/java/org/cmucreatelab/flutter_android/helpers/NamingHandler.java
+++ app/src/main/java/org/cmucreatelab/flutter_android/helpers/NamingHandler.java
@@ -9,7 +9,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Hashtable;
+import java.util.ArrayList;
 
 /**
  * Created by Steve on 6/9/2016.
@@ -23,16 +23,14 @@ public class NamingHandler {
 
 
     private Context context;
-    private Hashtable<String, String> firstNames;
-    private Hashtable<String, String> middleNames;
-    private Hashtable<String, String> lastNames;
+    private ArrayList<String> firstNames,middleNames,lastNames;
 
 
     public NamingHandler(Context context) {
         this.context = context;
-        this.firstNames = new Hashtable<>();
-        this.middleNames = new Hashtable<>();
-        this.lastNames = new Hashtable<>();
+        firstNames = new ArrayList<>();
+        middleNames = new ArrayList<>();
+        lastNames = new ArrayList<>();
 
         // init tables
         InputStream first = context.getResources().openRawResource(R.raw.first_names);
@@ -44,19 +42,16 @@ public class NamingHandler {
         try {
             for (char i : Constants.HEX_ALPHABET) {
                 for (char j : Constants.HEX_ALPHABET) {
-                    String ith = Character.toString(i);
-                    String jth = Character.toString(j);
-                    String hexString = ith.concat(jth);
                     String name;
                     try {
                         if ((name = brFirst.readLine()) != null) {
-                            firstNames.put(hexString, name);
+                            firstNames.add(name);
                         }
                         if ((name = brMiddle.readLine()) != null) {
-                            middleNames.put(hexString, name);
+                            middleNames.add(name);
                         }
                         if ((name = brLast.readLine()) != null) {
-                            lastNames.put(hexString, name);
+                            lastNames.add(name);
                         }
                     } catch (IOException e) {
                         e.printStackTrace();
@@ -74,18 +69,30 @@ public class NamingHandler {
 
     public String generateName(String mac) {
         String result = "unknown";
+        long mid;
+        int i,j,k,offset;
 
+        // expected input: "aa:bb:cc:dd:ee:ff" => "d:ee:ff"
         mac = mac.substring(9);
         String first;
         String middle;
         String last;
 
-        first = mac.substring(0,2);
-        middle = mac.substring(3,5);
-        last = mac.substring(6);
-        first = firstNames.get(first);
-        middle = middleNames.get(middle);
-        last = lastNames.get(last);
+        // grab bits from the MAC address (6 bits, 6 bits, 8 bits => last, middle, first)
+        i = Integer.parseInt(mac.substring(6),16);
+        mid = Long.parseLong(mac.substring(1,2).concat(mac.substring(3,5)),16);
+        k = (int)(mid / 64);
+        j = (int)(mid % 64);
+
+        // use the last 4 bits to "shift" all of the indices.
+        offset = i % 16;
+        i += offset;
+        j += offset;
+        k += offset;
+
+        first = firstNames.get(i);
+        middle = middleNames.get(j);
+        last = lastNames.get(k);
 
         if (first != null && middle != null && last != null) {
             first = first.concat(" ");
