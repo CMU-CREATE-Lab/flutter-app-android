900e1f152a4526c39e1368092070b65f27eafd9c
SteveFulton95 <sjf1008@sru.edu>
2017-03-20 13:50:34 -0400

    Added scroll views to the clean up dialogs

---
+++ app/src/main/java/org/cmucreatelab/flutter_android/ui/ExtendedHeightListView.java
@@ -0,0 +1,34 @@
+package org.cmucreatelab.flutter_android.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+/**
+ * Created by Steve on 3/20/2017.
+ */
+
+public class ExtendedHeightListView extends ListView {
+
+
+    public ExtendedHeightListView(Context context) {
+        super(context);
+    }
+
+
+    public ExtendedHeightListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+    public ExtendedHeightListView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+    @Override
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, expandSpec);
+    }
+}
