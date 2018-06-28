cce667e7b0aa2da3e736b4fc393af5e155ea39f4
mtasota <miketasota@gmail.com>
2018-06-28 10:28:32 -0400

    Display sensor icon and name

--- app/src/main/java/org/cmucreatelab/flutter_android/ui/dialogs/wizards/robot_outputs_wizard/ChooseSensorOutputDialogWizard.java
+++ app/src/main/java/org/cmucreatelab/flutter_android/ui/dialogs/wizards/robot_outputs_wizard/ChooseSensorOutputDialogWizard.java
@@ -8,11 +8,16 @@ import android.support.v7.internal.view.ContextThemeWrapper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import org.cmucreatelab.flutter_android.R;
 import org.cmucreatelab.flutter_android.classes.outputs.Servo;
 import org.cmucreatelab.flutter_android.classes.outputs.Speaker;
 import org.cmucreatelab.flutter_android.classes.outputs.TriColorLed;
+import org.cmucreatelab.flutter_android.classes.sensors.Sensor;
+import org.cmucreatelab.flutter_android.helpers.GlobalHandler;
 import org.cmucreatelab.flutter_android.helpers.static_classes.Constants;
 import org.cmucreatelab.flutter_android.ui.dialogs.wizards.BaseResizableDialogWizard;
 
@@ -43,6 +48,24 @@ public class ChooseSensorOutputDialogWizard extends BaseResizableDialogWizard {
     }
 
 
+    private void populateSensors(View view) {
+        Sensor sensors[] = GlobalHandler.getInstance(this.getActivity()).sessionHandler.getSession().getFlutter().getSensors();
+        ImageView sensor1 = (ImageView) view.findViewById(R.id.image_sensor_1);
+        ImageView sensor2 = (ImageView) view.findViewById(R.id.image_sensor_2);
+        ImageView sensor3 = (ImageView) view.findViewById(R.id.image_sensor_3);
+        TextView textSensor1 = (TextView) view.findViewById(R.id.text_sensor_1);
+        TextView textSensor2 = (TextView) view.findViewById(R.id.text_sensor_2);
+        TextView textSensor3 = (TextView) view.findViewById(R.id.text_sensor_3);
+
+        sensor1.setImageResource(sensors[0].getGreenImageId());
+        sensor2.setImageResource(sensors[1].getGreenImageId());
+        sensor3.setImageResource(sensors[2].getGreenImageId());
+        textSensor1.setText(sensors[0].getSensorTypeId());
+        textSensor2.setText(sensors[1].getSensorTypeId());
+        textSensor3.setText(sensors[2].getSensorTypeId());
+    }
+
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         super.onCreateDialog(savedInstanceState);
@@ -52,6 +75,7 @@ public class ChooseSensorOutputDialogWizard extends BaseResizableDialogWizard {
         builder.setView(view);
         ButterKnife.bind(this, view);
         this.dialogView = view;
+        populateSensors(view);
 
         return builder.create();
     }
