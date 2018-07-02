eb63b50d3ec7856c65ee034b921f31099030c9a5
mtasota <miketasota@gmail.com>
2018-07-02 13:39:59 -0400

    wizards: made generateSettings method abstract; needs implemented in each subclass

--- app/src/main/java/org/cmucreatelab/flutter_android/ui/dialogs/wizards/robot_outputs_wizard/OutputWizard.java
+++ app/src/main/java/org/cmucreatelab/flutter_android/ui/dialogs/wizards/robot_outputs_wizard/OutputWizard.java
@@ -3,12 +3,9 @@ package org.cmucreatelab.flutter_android.ui.dialogs.wizards.robot_outputs_wizard
 import android.util.Log;
 
 import org.cmucreatelab.flutter_android.activities.RobotActivity;
-import org.cmucreatelab.flutter_android.classes.outputs.Output;
+import org.cmucreatelab.flutter_android.classes.outputs.FlutterOutput;
 import org.cmucreatelab.flutter_android.classes.relationships.NoRelationship;
-import org.cmucreatelab.flutter_android.classes.relationships.Proportional;
 import org.cmucreatelab.flutter_android.classes.relationships.Relationship;
-import org.cmucreatelab.flutter_android.classes.settings.Settings;
-import org.cmucreatelab.flutter_android.classes.settings.SettingsProportional;
 import org.cmucreatelab.flutter_android.helpers.static_classes.Constants;
 import org.cmucreatelab.flutter_android.ui.dialogs.robots_tab.outputs.BaseOutputDialog;
 import org.cmucreatelab.flutter_android.ui.dialogs.wizards.BaseResizableDialogWizard;
@@ -20,7 +17,7 @@ import java.io.Serializable;
  * Created by mike on 6/29/18.
  */
 
-public abstract class OutputWizard<T extends Output> extends Wizard {
+public abstract class OutputWizard<T extends FlutterOutput> extends Wizard {
 
     private RobotActivity activity;
     private T output;
@@ -32,24 +29,6 @@ public abstract class OutputWizard<T extends Output> extends Wizard {
         public int selectedSensorPort=0,
                 outputMin=0,
                 outputMax=100;
-    }
-
-
-    private static void generateSettings(OutputWizard.State currentState, Output output) {
-        // TODO @tasota avoid crash when something wasn't set
-        if (currentState.relationshipType.getClass() == NoRelationship.class) {
-            currentState.relationshipType = Proportional.getInstance();
-        }
-        if (currentState.selectedSensorPort < 1 || currentState.selectedSensorPort > 3) {
-            currentState.selectedSensorPort = 1;
-        }
-
-        SettingsProportional newSettings = SettingsProportional.newInstance(output.getSettings());
-        newSettings.setSensorPortNumber(currentState.selectedSensorPort);
-        newSettings.setOutputMin(currentState.outputMin);
-        newSettings.setOutputMax(currentState.outputMax);
-        output.setSettings( Settings.newInstance(newSettings, currentState.relationshipType) );
-        output.setIsLinked(true, output);
     }
 
 
@@ -93,6 +72,9 @@ public abstract class OutputWizard<T extends Output> extends Wizard {
         currentState.currentDialog.dismiss();
         currentState.currentDialog = nextDialog;
     }
+
+
+    public abstract void generateSettings(OutputWizard.State currentState, T output);
 
 
     public abstract BaseOutputDialog generateOutputDialog(T output, RobotActivity activity);
--- app/src/main/java/org/cmucreatelab/flutter_android/ui/dialogs/wizards/robot_outputs_wizard/servo/ServoWizard.java
+++ app/src/main/java/org/cmucreatelab/flutter_android/ui/dialogs/wizards/robot_outputs_wizard/servo/ServoWizard.java
@@ -1,7 +1,12 @@
 package org.cmucreatelab.flutter_android.ui.dialogs.wizards.robot_outputs_wizard.servo;
 
 import org.cmucreatelab.flutter_android.activities.RobotActivity;
+import org.cmucreatelab.flutter_android.classes.outputs.FlutterOutput;
 import org.cmucreatelab.flutter_android.classes.outputs.Servo;
+import org.cmucreatelab.flutter_android.classes.relationships.NoRelationship;
+import org.cmucreatelab.flutter_android.classes.relationships.Proportional;
+import org.cmucreatelab.flutter_android.classes.settings.Settings;
+import org.cmucreatelab.flutter_android.classes.settings.SettingsProportional;
 import org.cmucreatelab.flutter_android.ui.dialogs.robots_tab.outputs.BaseOutputDialog;
 import org.cmucreatelab.flutter_android.ui.dialogs.robots_tab.outputs.servo.ServoDialog;
 import org.cmucreatelab.flutter_android.ui.dialogs.wizards.robot_outputs_wizard.OutputWizard;
@@ -15,6 +20,24 @@ public class ServoWizard extends OutputWizard<Servo> {
 
     public ServoWizard(RobotActivity activity, Servo servo) {
         super(activity, Servo.newInstance(servo));
+    }
+
+
+    public void generateSettings(OutputWizard.State currentState, Servo output) {
+        // TODO @tasota avoid crash when something wasn't set
+        if (currentState.relationshipType.getClass() == NoRelationship.class) {
+            currentState.relationshipType = Proportional.getInstance();
+        }
+        if (currentState.selectedSensorPort < 1 || currentState.selectedSensorPort > 3) {
+            currentState.selectedSensorPort = 1;
+        }
+
+        SettingsProportional newSettings = SettingsProportional.newInstance(output.getSettings());
+        newSettings.setSensorPortNumber(currentState.selectedSensorPort);
+        newSettings.setOutputMin(currentState.outputMin);
+        newSettings.setOutputMax(currentState.outputMax);
+        output.setSettings( Settings.newInstance(newSettings, currentState.relationshipType) );
+        output.setIsLinked(true, output);
     }
 
 
