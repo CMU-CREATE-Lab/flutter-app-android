c2895d101cbc8ead4930adf6fb10c67bd91fbd49
Parv Shrivastava <parvshrivastava@gmail.com>
2018-08-08 10:12:17 -0400

    Added first unit test for flutter protocol

--- app/src/androidTest/java/org/cmucreatelab/flutter_android/ApplicationTest.java
+++ app/src/androidTest/java/org/cmucreatelab/flutter_android/ApplicationTest.java
@@ -3,6 +3,8 @@ package org.cmucreatelab.flutter_android;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+import org.junit.Rule;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
